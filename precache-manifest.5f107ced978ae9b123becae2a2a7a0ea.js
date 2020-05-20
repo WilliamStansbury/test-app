fed1ec7ac4fd96feb3dbe4f4f54e8a9270fc0bff
William Stansbury <williamstansbury@Williams-Mac.local>
2020-05-19 17:15:01 -0700

    Updates

---
+++ precache-manifest.5f107ced978ae9b123becae2a2a7a0ea.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230247f6111ae40cf9c5f14c3ce04bc4",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "abd35de04de3ed7b0116",
+    "url": "/test-app/static/css/main.40fbe926.chunk.css"
+  },
+  {
+    "revision": "e28cb8ec613881d4e507",
+    "url": "/test-app/static/js/2.8fda3078.chunk.js"
+  },
+  {
+    "revision": "01fef312c49c91e5646b45e7b647909d",
+    "url": "/test-app/static/js/2.8fda3078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd35de04de3ed7b0116",
+    "url": "/test-app/static/js/main.4de7f016.chunk.js"
+  },
+  {
+    "revision": "b3c08ce8953bee725834",
+    "url": "/test-app/static/js/runtime-main.39e027db.js"
+  },
+  {
+    "revision": "9f6683332da2cae3df704e585e3275fb",
+    "url": "/test-app/static/media/NotreDame2.9f668333.jpg"
+  },
+  {
+    "revision": "7f723504f7868009181995c0018ba99b",
+    "url": "/test-app/static/media/PinkDress2.7f723504.jpg"
+  },
+  {
+    "revision": "2bae0e4a87d387f4ae2d420cb96aa145",
+    "url": "/test-app/static/media/amelie.2bae0e4a.jpg"
+  },
+  {
+    "revision": "42caec18440f0a339d6031632e2e8b82",
+    "url": "/test-app/static/media/bruges.42caec18.jpg"
+  },
+  {
+    "revision": "d63d60d855e39bcfc7953b47dc4bdf5d",
+    "url": "/test-app/static/media/fantastic.d63d60d8.jpg"
+  },
+  {
+    "revision": "60703cbd3ce579f763fc4d0d42273d27",
+    "url": "/test-app/static/media/fifth.60703cbd.jpg"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/test-app/static/media/github2.0ba2aa20.png"
+  },
+  {
+    "revision": "ef30a428ad0847473b4dc44099c00ab2",
+    "url": "/test-app/static/media/heathers.ef30a428.jpg"
+  },
+  {
+    "revision": "036e48200911486c1633f7b60fa1dceb",
+    "url": "/test-app/static/media/kiev2.036e4820.jpg"
+  },
+  {
+    "revision": "be4e3bfeb4a1719c913b8509a9f2c04d",
+    "url": "/test-app/static/media/londoneye.be4e3bfe.JPG"
+  },
+  {
+    "revision": "4ec82bbc668a968724541cf1a6a280a2",
+    "url": "/test-app/static/media/londoneye2.4ec82bbc.jpg"
+  },
+  {
+    "revision": "586b06785018ad1c729ee734f9c28269",
+    "url": "/test-app/static/media/right.586b0678.jpg"
+  },
+  {
+    "revision": "9231c88b2fd8f3d3d72b258aa8fe9edb",
+    "url": "/test-app/static/media/scott.9231c88b.jpg"
+  },
+  {
+    "revision": "31badc50b2154e22703ba73382623aa2",
+    "url": "/test-app/static/media/shadows.31badc50.jpg"
+  },
+  {
+    "revision": "6f5c2835ee84ebdfdccb5b47caaffc90",
+    "url": "/test-app/static/media/smolShop.6f5c2835.jpg"
+  },
+  {
+    "revision": "13be4845b88494b0b1d9c1f3860f3aec",
+    "url": "/test-app/static/media/spiderman.13be4845.jpg"
+  },
+  {
+    "revision": "a9048325c3bab7e63ac15dd6b3cdaeee",
+    "url": "/test-app/static/media/train2.a9048325.jpg"
+  },
+  {
+    "revision": "72b4606634cb1e180d1b08800a3c377a",
+    "url": "/test-app/static/media/tucker.72b46066.jpg"
+  },
+  {
+    "revision": "92eac3dce8ae0acbea6524e240f98e92",
+    "url": "/test-app/static/media/whiplash.92eac3dc.jpg"
+  }
+]);
