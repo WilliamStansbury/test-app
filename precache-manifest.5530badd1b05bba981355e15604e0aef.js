7bd32e075650d83ace68ba9fcfe95faf681e238f
William Stansbury <williamstansbury@Williams-Mac.local>
2020-06-02 22:56:47 -0700

    Updates

---
+++ precache-manifest.5530badd1b05bba981355e15604e0aef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9d3497991a517ca57dee460b6354a8",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "780db3188ab2617e4618",
+    "url": "/test-app/static/css/main.9a907c52.chunk.css"
+  },
+  {
+    "revision": "a4587320de615f7844d4",
+    "url": "/test-app/static/js/2.31ec0336.chunk.js"
+  },
+  {
+    "revision": "01fef312c49c91e5646b45e7b647909d",
+    "url": "/test-app/static/js/2.31ec0336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780db3188ab2617e4618",
+    "url": "/test-app/static/js/main.24c43c69.chunk.js"
+  },
+  {
+    "revision": "b3c08ce8953bee725834",
+    "url": "/test-app/static/js/runtime-main.39e027db.js"
+  },
+  {
+    "revision": "9f6683332da2cae3df704e585e3275fb",
+    "url": "/test-app/static/media/NotreDame2.9f668333.jpg"
+  },
+  {
+    "revision": "7f723504f7868009181995c0018ba99b",
+    "url": "/test-app/static/media/PinkDress2.7f723504.jpg"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/test-app/static/media/github2.0ba2aa20.png"
+  },
+  {
+    "revision": "036e48200911486c1633f7b60fa1dceb",
+    "url": "/test-app/static/media/kiev2.036e4820.jpg"
+  },
+  {
+    "revision": "be4e3bfeb4a1719c913b8509a9f2c04d",
+    "url": "/test-app/static/media/londoneye.be4e3bfe.JPG"
+  },
+  {
+    "revision": "4ec82bbc668a968724541cf1a6a280a2",
+    "url": "/test-app/static/media/londoneye2.4ec82bbc.jpg"
+  },
+  {
+    "revision": "a9048325c3bab7e63ac15dd6b3cdaeee",
+    "url": "/test-app/static/media/train2.a9048325.jpg"
+  }
+]);
